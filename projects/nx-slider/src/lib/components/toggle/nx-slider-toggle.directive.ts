ca95e827ef0f0ab8aac2cc1c646725a938a26ecb
Maxim Milovanov <mma29121983@gmail.com>
2019-01-07 15:05:42 -0500

    feat: adds attribute on toggler when slider open

--- projects/nx-slider/src/lib/components/toggle/nx-slider-toggle.directive.ts
+++ projects/nx-slider/src/lib/components/toggle/nx-slider-toggle.directive.ts
@@ -1,25 +1,54 @@
-import {Directive, Input, OnInit, HostListener} from '@angular/core';
+import {
+  Directive,
+  Input,
+  OnInit,
+  HostListener,
+  Renderer2,
+  OnDestroy,
+  ElementRef,
+} from '@angular/core';
 import {INxSliderState} from '../../types/nx-slider-state.interface';
 import {NxSliderService} from '../../services/nx-slider.service';
 import {INxSliderActions} from '../../types/nx-slider-actions.interface';
-import {take} from 'rxjs/operators';
+import {Subscription} from 'rxjs';
 
 @Directive({
   selector: '[nxSliderToggle]',
 })
-export class NxSliderToggleDirective implements OnInit {
+export class NxSliderToggleDirective implements OnInit, OnDestroy {
   @Input() state?: INxSliderState & INxSliderActions | null = null;
+  private isOpen: boolean = false; // tslint:disable-line:no-inferrable-types
+  private isOpenSubscription: Subscription;
 
-  constructor(private defaultState: NxSliderService) {}
+  constructor(
+    private defaultState: NxSliderService,
+    private renderer: Renderer2,
+    private el: ElementRef,
+  ) {}
 
   ngOnInit() {
     this.state = this.state || this.defaultState;
+    this.isOpenSubscription = this.state.isOpen.subscribe((value: boolean) => {
+      this.isOpen = value;
+      if (value) {
+        this.renderer.setAttribute(
+          this.el.nativeElement,
+          'nx-slider-open',
+          'true',
+        );
+      } else {
+        this.renderer.removeAttribute(this.el.nativeElement, 'nx-slider-open');
+      }
+    });
+  }
+  ngOnDestroy() {
+    if (this.isOpenSubscription) {
+      this.isOpenSubscription.unsubscribe();
+    }
   }
 
   @HostListener('click')
   private onClick(): void {
-    this.state.isOpen.pipe(take(1)).subscribe((value: boolean) => {
-      return value ? this.state.onClose() : this.state.onOpen();
-    });
+    return this.isOpen ? this.state.onClose() : this.state.onOpen();
   }
 }
